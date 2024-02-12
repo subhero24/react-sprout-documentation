3ca65d60ff6e8e7c60fe510d1442f4ee3d521fd3
subhero24 <github@bvdsoftware.be>
2024-02-12 21:23:05 +0100

    Deploy fixes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,9 @@
+import Image from 'next/image'
+
 export function Logo() {
 	return (
 		<div className="flex items-center gap-4">
-			<img className="h-8 w-8" src="/react-sprout.png" />
+			<Image className="h-8 w-8" src="/react-sprout.png" alt="react sprout" />
 			<span className="text-xl font-semibold">React Sprout</span>
 		</div>
 	)
