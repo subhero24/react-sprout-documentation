5f9cfb50aed47fd0b79eb6e7e01ca1b47b00f725
subhero24 <github@bvdsoftware.be>
2024-02-12 21:06:28 +0100

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+/** @type {import('prettier').Options} */
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  useTabs: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+export function Logo() {
+	return (
+		<div className="flex items-center gap-4">
+			<img className="h-8 w-8" src="/react-sprout.png" />
+			<span className="text-xl font-semibold">React Sprout</span>
+		</div>
+	)
+}
